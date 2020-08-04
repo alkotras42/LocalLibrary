b4d439a2bbd915fd88d08aeb77fbb548fc7e332a
alkotras42 <kirill.stogniy@mail.ru>
2020-08-04 04:13:26 +0300

    add books page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.contrib import admin
 from . import views
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
 
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from .models import BookInstance, Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
-    num_books = Book.objects.all().count()
+    num_books = Book.objects.exclude(title="").count()
     num_instance = BookInstance.objects.all().count()
 
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    num_authors = Author.objects.count()
+    num_authors = Author.objects.exclude(first_name="").count()
 
     return render(
         request,
         "index.html",
         context={'num_books': num_books, 'num_instance': num_instance, 'num_instances_available': num_instances_available, 'num_authors': num_authors}
     )
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
