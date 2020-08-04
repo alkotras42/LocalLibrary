bf384ffe729f4821e7c190b63f352212fde127a1
alkotras42 <kirill.stogniy@mail.ru>
2020-08-05 01:01:59 +0300

    add session

--- catalog/views.py
+++ catalog/views.py
@@ -9,10 +9,14 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.exclude(first_name="").count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     return render(
         request,
         "index.html",
-        context={'num_books': num_books, 'num_instance': num_instance, 'num_instances_available': num_instances_available, 'num_authors': num_authors}
+        context={'num_books': num_books, 'num_instance': num_instance, 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_visits': num_visits}
     )
 
 class BookListView(generic.ListView):
