a20d61e15a95f8179f56496c3241552cdb1ecc6f
alkotras42 <kirill.stogniy@mail.ru>
2020-08-09 02:25:57 +0300

    add borrowed books page

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
 import uuid
 
 class Genre(models.Model):
@@ -67,6 +69,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -79,6 +82,12 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
+
+    @property
+    def is_overdue(self):
+        if self.due_back and (date.today() > self.due_back):
+            return True
+        return False
         
 
     def __str__(self):
